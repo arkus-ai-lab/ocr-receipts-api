dab58fd7730350e87041960d57af125f3d0a77eb
Ana Pacheco <144283863+AnaArkus@users.noreply.github.com>
2024-04-18 07:40:15 -0600

    add config variables and envsample

--- src/ocr_text/api/views.py
+++ src/ocr_text/api/views.py
@@ -4,6 +4,7 @@ import logging
 from rest_framework.parsers import MultiPartParser, FormParser
 
 from ocr_text.api.serializers import Receipt
+from utilities.document_ai import get_documents
 
 class OCRTextView(generics.GenericAPIView):
     """   
@@ -17,7 +18,9 @@ class OCRTextView(generics.GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            documents = get_documents()
+
+            return Response(documents, status=status.HTTP_201_CREATED)
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
--- src/utilities/config.py
+++ src/utilities/config.py
@@ -2,3 +2,10 @@ import os
 
 CREDENTIALS = os.getenv("HUGGINGFACE_ACCESS_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+PROJECT_SETUP = {
+    "project_id": os.getenv("PROJECT_ID"),
+    "location": os.getenv("LOCATION"),
+    "processor_id": os.getenv("PROCESSOR_ID"),
+    "endpoint": os.getenv("ENDPOINT"),
+}
